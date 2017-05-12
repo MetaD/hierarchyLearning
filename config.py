6b9cde3e055da9dffdbd715f9531f4dcf2aaf6ff
MetaD <mengdu@umich.edu>
2017-05-12 16:00:38 -0700

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,8 @@ NUM_CYCLES_PER_BLOCK_TRAIN = 2
 NUM_CYCLES_PER_BLOCK_TEST = 1
 TRAIN_PAIRS = [(0, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 7), (7, 8)]
 TEST_PAIRS = [(1, 3), (1, 4), (2, 4), (2, 5), (3, 5), (3, 6), (4, 6), (4, 7)]
-POINTS = 20
+TRAIN_POINTS = 10
+TEST_POINTS = 100
 MAX_ADDITIONAL_BLOCKS = 4
 # Paths
 IMG_FOLDER = 'img/'
@@ -27,15 +28,22 @@ INSTR_1 = ['We\'re interested in individual differences in how people learn abou
 INSTR_2 = 'Thank you for participating!'
 
 INSTR_TRAIN = ['Get ready for Training trials.',
-               'In training trials, press F if you think the person on the left has more power.\n\n' +
+               'In Training Trials, you\'ll see pairs of people who are most similar in terms of how much power ' +
+               'they have in the organization.\n\n' + 
+               'Press F if you think the person on the left has more power.\n\n' +
                'Press J if you think the person on the right has more power.\n\n' +
-               'If you respond correctly, you\'ll win 20 points. If you respond incorrectly, you\'ll lose 20 points.']
+               'If you respond correctly, you\'ll win ' + str(TRAIN_POINTS) + ' points. ' +
+               'If you respond incorrectly, you\'ll lose ' + str(TRAIN_POINTS) + ' points.']
 INSTR_TEST = ['Get ready for Test trials',
-              'In test trials, press F or J to choose the person who you think has more power.\n\n' +
+              'In Test Trials, you\'ll be presented with pairs of people who are more different from each other ' +
+              '(compared to the Training Trials) in terms of how much power they have.' +
+              'Press F or J to choose the person who you think has more power.\n\n' +
               'You\'ll notice that there will be pairs presented together that aren\'t presented during training ' +
               'trials - here you\'ll have to use your judgement to choose the correct one.\n\n' +
               'Feedback is not provided in test trials, however, your responses count just as training trials for ' +
-              'computing your final score.',
+              'computing your final score.\n\n' +
+              'If you respond correctly, you\'ll win ' + str(TEST_POINTS) + ' points. ' +
+              'If you respond incorrectly, you\'ll lose ' + str(TEST_POINTS) + ' points.',
               'You will also be asked to rate your confidence in your choices during test trials on a scale of 1-3:\n' +
               '\n1 = You\'re guessing entirely\n' +
               '2 = You have some idea but are not sure\n' +
