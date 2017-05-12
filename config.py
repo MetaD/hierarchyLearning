8f0e9ba3cabc1d83ad38f2a838797ef69d443805
MetaD <mengdu@umich.edu>
2017-05-12 13:34:21 -0700

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,6 @@ NUM_CYCLES_PER_BLOCK_TEST = 1
 TRAIN_PAIRS = [(0, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 7), (7, 8)]
 TEST_PAIRS = [(1, 3), (1, 4), (2, 4), (2, 5), (3, 5), (3, 6), (4, 6), (4, 7)]
 POINTS = 20
-PASSING_ACCURACY = 0.9
 MAX_ADDITIONAL_BLOCKS = 4
 # Paths
 IMG_FOLDER = 'img/'
