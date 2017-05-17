e73730bd4740062abe2126f56e37df50fb72ffff
Meng Du <mengdu@umich.edu>
2017-05-16 20:23:19 -0700

    new data checker

--- data_checker.py
+++ data_checker.py
@@ -1,63 +1,36 @@
-import json
-import pandas as pd
-import numpy as np
+import os
+from data_utilities import *
 
-f = open('data/georgia.txt', 'r')
 
-train_data, test_data = [], []
-for ln_num, ln in enumerate(f):
+DATA_FOLDER = 'data/'
+CSV_TRAIN = 'learning_data_train.csv'
+CSV_TEST = 'learning_data_test.csv'
 
-    # load in data file
-    jdict = json.loads(ln)
+all_trainings, all_tests = [], []
+for datafile in os.listdir(DATA_FOLDER):
+    if not datafile.endswith('.txt') or not datafile[0].isdigit():
+        continue
+    sdata = load_json(DATA_FOLDER + datafile, multiple_obj=True)
+    training, test = [datafile[:-4]], [datafile[:-4]]
+    for trial in sdata:
+        if 'block' in trial:
+            block_type = trial['block']
+            data_list = training if 'train' in block_type else test
+            correct = 1 if trial['correct'] else 0
+            try:
+                data_list[int(block_type.split('_')[0]) + 1] += correct
+            except IndexError:
+                data_list.append(correct)
+    all_trainings.append(training)
+    all_tests.append(test)
 
-    # extract demographic variables from relevant line
-    if ("Gender" in jdict):
-        demographics = jdict
 
-    # extract experiment info from relevant line
-    elif ("psychopyVersion" in jdict) or (ln_num == 1):
-        exp_info = jdict
+def write(csvname, data):
+    with open(csvname, 'w') as outfile:
+        writer = csv.writer(outfile, delimiter=',')
+        writer.writerow(['ID'] + ['block_' + str(i) for i in range(16)])
+        for line in data:
+            writer.writerow(line)
 
-    # extract this participant's stimulus hierarchy order
-    elif "0" in jdict:
-        stim_ordering = jdict
-
-    # extract data ouput from training and testing blocks separately
-    elif "block" in jdict:
-        block_type = jdict["block"].split('_')
-        if block_type[1] == 'train':
-            jdict['train_block#'] = block_type[0]
-            train_data.append(jdict)
-        elif block_type[1] == 'test':
-            jdict['test_block#'] = block_type[0]
-            test_data.append(jdict)
-        else:
-            raise Exception("Error: ooooops!")
-
-    # don't need to load in block earnings (redundant)
-    elif "block_earnings" in jdict:
-        pass
-
-    else:
-        raise Exception("Error: something unexpected!")
-
-# load training and testing blocks data into Pandas data frames
-train_data_df = pd.DataFrame.from_records(train_data)
-test_data_df = pd.DataFrame.from_records(test_data)
-
-# char -> integer
-train_data_df["train_block#"] = train_data_df["train_block#"].astype(int)
-test_data_df["test_block#"] = test_data_df["test_block#"].astype(int)
-
-# omit some colulmns for simplicity of output during piloting
-train_data_df = train_data_df.drop(["response", "rt"], axis=1)
-test_data_df = test_data_df.drop(["response", "rt"], axis=1)
-
-# to get sum of correct responses per block
-a = train_data_df.groupby("train_block#")
-training_trial_summary = a.aggregate(np.sum)
-print training_trial_summary
-
-b = test_data_df.groupby("test_block#")
-test_data_summary = b.aggregate(np.sum)
-print test_data_summary
+write(CSV_TRAIN, all_trainings)
+write(CSV_TEST, all_tests)
