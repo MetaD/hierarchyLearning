c8ed1bb7a08891780387c26718765d084ad6170a
Meng Du <mengdu@umich.edu>
2017-06-20 17:04:15 -0700

    s58; combined data file

--- data_checker.py
+++ data_checker.py
@@ -3,34 +3,29 @@ from data_utilities import *
 
 
 DATA_FOLDER = 'data/'
-CSV_TRAIN = 'learning_data_train.csv'
-CSV_TEST = 'learning_data_test.csv'
+CSV_FILENAME = 'learning_data.csv'
 
-all_trainings, all_tests = [], []
+all_data = []
 for datafile in os.listdir(DATA_FOLDER):
     if not datafile.endswith('.txt') or not datafile[0].isdigit():
         continue
     sdata = load_json(DATA_FOLDER + datafile, multiple_obj=True)
-    training, test = [datafile[:-4]], [datafile[:-4]]
+    training, test = [datafile[:-4], 'train (16)'], [datafile[:-4], 'test (8)']
     for trial in sdata:
         if 'block' in trial:
             block_type = trial['block']
             data_list = training if 'train' in block_type else test
-            correct = 1 if trial['correct'] else 0
+            correct = int(trial['correct'])
             try:
-                data_list[int(block_type.split('_')[0]) + 1] += correct
+                data_list[int(block_type.split('_')[0]) + 2] += correct
             except IndexError:
                 data_list.append(correct)
-    all_trainings.append(training)
-    all_tests.append(test)
+    all_data.append(training)
+    all_data.append(test)
 
 
-def write(csvname, data):
-    with open(csvname, 'w') as outfile:
-        writer = csv.writer(outfile, delimiter=',')
-        writer.writerow(['id'] + ['block_' + str(i) for i in range(16)])
-        for line in data:
-            writer.writerow(line)
-
-write(CSV_TRAIN, all_trainings)
-write(CSV_TEST, all_tests)
+with open(CSV_FILENAME, 'w') as outfile:
+    writer = csv.writer(outfile, delimiter=',')
+    writer.writerow(['id', 'block_type'] + ['block_' + str(i) for i in range(16)])
+    for line in all_data:
+        writer.writerow(line)
